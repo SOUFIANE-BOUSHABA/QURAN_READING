3dabd435d24bedb46065c20dc81d2a5e619406fa
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-05-31 20:44:50 +0000

    this commit is for make the structer of project and  make the code of the header

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+// app/about/page.js
+import Layout from '../layouts/layout';
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <div className="p-4">
+        <h2 className="text-xl">About Us</h2>
+        <p>This is the about page of the Quran Reading App.</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default AboutPage;
