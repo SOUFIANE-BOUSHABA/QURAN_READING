144227712e63c061b194e624fe17fdebf81e0bed
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-06-04 13:35:46 +0000

    this commit is for fix the footer

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import {
   RxDiscordLogo,
   RxGithubLogo,
-  RxInstagramLogo,
   RxTwitterLogo,
   RxLinkedinLogo,
 } from "react-icons/rx";
-import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
