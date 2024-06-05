11b182ff11013e136182a07562d4bbe4d86756c7
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-06-05 16:00:34 +0000

    this commit is for creating the code of make tafsir ayah

--- components/read/Modal.jsx
+++ components/read/Modal.jsx
@@ -47,7 +47,7 @@ const Modal = ({ isOpen, onClose, tafsir, fetchTafsir }) => {
 
   const handleSurahChange = (event) => {
     setSelectedSurah(event.target.value);
-    setSelectedAyah(''); 
+    setSelectedAyah('');
   };
 
   const handleAyahChange = (event) => {
@@ -59,34 +59,44 @@ const Modal = ({ isOpen, onClose, tafsir, fetchTafsir }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed container max-w-[900px] mx-auto min-h-[500px] inset-0 flex items-center text-slate-900 justify-center bg-black bg-opacity-50 z-[30]">
-      <div className="bg-white p-8 rounded-lg relative">
-        <div className="flex items-start gap-20 justify-between mb-4">
-
-        
-        <div className="flex gap-4 mb-4 items-center">
-          <label htmlFor="surah" className="text-lg">Select Surah:</label>
-          <select id="surah" className="border border-gray-300 rounded-md p-2" value={selectedSurah} onChange={handleSurahChange}>
-            <option value="">Select Surah</option>
-            {surahs.map((surah) => (
-              <option key={surah.number} value={surah.number}>{surah.name}</option>
-            ))}
-          </select>
-          <label htmlFor="ayah" className="text-lg">Select Ayah:</label>
-          <select id="ayah" className="border border-gray-300 rounded-md p-2" value={selectedAyah} onChange={handleAyahChange} disabled={!selectedSurah}>
-            <option value="">Select Ayah</option>
-            {ayahs.map((ayah) => (
-              <option key={ayah.verse_key} value={ayah.verse_key.split(':')[1]}>{ayah.verse_key}</option>
-            ))}
-          </select>
+    <div className="fixed inset-0 flex items-center justify-center text-zinc-700 bg-black bg-opacity-50 z-[30] px-4">
+      <div className="bg-white p-8 rounded-lg relative w-full max-w-[900px] mx-auto min-h-[300px]">
+        <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-4 gap-4 md:gap-20">
+          <button
+            className="md:absolute md:top-4 md:right-4 text-slate-900 order-1 md:order-2 rounded-full w-10 h-10 bg-zinc-100 hover:bg-zinc-200 flex items-center justify-center"
+            onClick={onClose}
+          >
+            &times;
+          </button>
+          <div className="flex flex-col md:flex-row gap-4 text-black items-center w-full order-2 md:order-1">
+            <label htmlFor="surah" className="text-lg">Select Surah:</label>
+            <select
+              id="surah"
+              className="border border-gray-300 rounded-md p-2 w-full md:w-auto"
+              value={selectedSurah}
+              onChange={handleSurahChange}
+            >
+              <option value="">Select Surah</option>
+              {surahs.map((surah) => (
+                <option key={surah.number} value={surah.number}>{surah.name}</option>
+              ))}
+            </select>
+            <label htmlFor="ayah" className="text-lg">Select Ayah:</label>
+            <select
+              id="ayah"
+              className="border border-gray-300 rounded-md p-2 w-full md:w-auto"
+              value={selectedAyah}
+              onChange={handleAyahChange}
+              disabled={!selectedSurah}
+            >
+              <option value="">Select Ayah</option>
+              {ayahs.map((ayah) => (
+                <option key={ayah.verse_key} value={ayah.verse_key.split(':')[1]}>{ayah.verse_key}</option>
+              ))}
+            </select>
+          </div>
         </div>
-
-        <button className=" top-0 right-0  text-slate-900 rounded-full w-[70px] h-[40px] bg-zinc-100 hover:bg-zinc-200" onClick={onClose}>
-          x
-        </button>
-        </div>
-      
-        <p className='text-right'>{tafsir}</p>
+        <p className="text-right">{tafsir}</p>
       </div>
     </div>
   );
