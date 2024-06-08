d6134452fe8cd90f71f281c2a30714a08c055df4
SOUFIANE-BOUSHABA <soufianboushaba12@gmail.com>
2024-06-08 19:38:36 +0000

    this commit is for fix  the padding of  the fillet r and search

--- components/read/SearchFilter.jsx
+++ components/read/SearchFilter.jsx
@@ -10,17 +10,22 @@ const SearchFilter = ({ onSearch, onFilter }) => {
   };
 
   return (
-    <div className="flex container px-4 mx-auto  gap-4 justify-end mb-4 mt-8 z-[20]">
+    <div className="flex container px-4 flex-wrap  mx-auto gap-4 justify-end mb-4 mt-8 z-[20]">
+      <label htmlFor="search-input" className="hidden">Search</label>
       <input
+        id="search-input"
         type="text"
         placeholder="ابحث عن سورة..."
         value={searchTerm}
         onChange={handleSearchChange}
-        className="mb-4 p-2 shadow-lg border-2 border-[#2A0E61] rounded h-10 w-96 text-right bg-transparent text-white placeholder-white"
+        className="mb-4 p-2 shadow-lg border-2 border-[#2A0E61] rounded h-10 w-full max-w-[500px] text-right bg-transparent text-white placeholder-white"
       />
+      
+      <label htmlFor="filter-select" className="hidden">Filter</label>
       <select 
+        id="filter-select"
         onChange={(e) => onFilter(e.target.value)} 
-        className="p-2 shadow-lg border-2 border-[#2A0E61] rounded h-10 w-96 text-right bg-transparent text-white">
+        className="p-2 shadow-lg border-2 border-[#2A0E61] rounded h-10 w-full   max-w-[500px] text-right bg-transparent text-white">
         <option value="" className="bg-gray-800">الكل</option>
         <option value="makkah" className="bg-gray-800">السور المكية</option>
         <option value="madinah" className="bg-gray-800">السور المدنية</option>
